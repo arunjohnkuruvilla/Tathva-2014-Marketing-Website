2bcb639653fe0f0763aefffb62a5d6816f202725
noonetoogreat <arunjohnkuruvilla@gmail.com>
2014-05-27 00:54:06 +0530

    second commit:trial

--- index.php
+++ index.php
@@ -220,7 +220,6 @@
             <div data-src="img/slideshow/image2.jpg"></div>
             <div data-src="img/slideshow/image1.jpg"></div>
             <div data-src="img/slideshow/image3.jpg"></div>
-            <div data-src="img/slideshow/image3.jpg"></div>
         </div>
     </div>
     <!--BACKGROUND SLIDESHOW ENDS HERE-->
