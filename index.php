eee80b42a4bc61661fa88fad9dd519c415e56c4f
noonetoogreat <arunjohnkuruvilla@gmail.com>
2014-05-27 00:15:02 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" > 
+    <title>Tathva '14</title> 
+    <meta name="description" content=""> 
+    <meta name="keywords" content="tathva, tathva14, tathva '14, marketing">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="">
+
+    <!--STYLESHEETS--> 
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/circular-buttons.css" />
+    <link rel="stylesheet" type="text/css" href="css/font.css" />
+
+    <!--BACKGROUND SLIDE-->
+    <link rel='stylesheet' id='fluid_dg-css'  href='css/slide.css' type='text/css' media='all' /> 
+    
+    <!-- GALLERY -->
+    <link rel="stylesheet" type="text/css" href="css/swipebox.css" />
+    <link rel="stylesheet" type="text/css" href="css/gallery.css" />
+
+    <!--FAVICON-->
+    <link rel="shortcut icon" href="img/favicon.ico" >
+    
+    <!--FACEBOOK BUTTONS-->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!--SPONSOR LIST FOR PREVIOUS SPONSORS TAB-->
+    <?php include'./include/sponsor-list.txt';?>
+</head>
+
+<body>
+
+    <!--NAVIGATION BAR-->
+    <nav class="navbar-fixed-top">
+    <div id="facebook-like"><div class="fb-like" data-href="https://www.facebook.com/tathva" data-width="50" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div></div>
+    	<div id="tathva13"><a href="http://tathva.org/2013/">TATHVA '13</a></div>
+        <div id="photo-gallery"><a href="gallery.php">GALLERY</a></div>
+        <div id="previous"><a href="sponsor.php">PREVIOUS SPONSORS</a></div>
+    </nav>
+    <!--NAVIGATION BAR ENDS HERE-->
+
+    <!--CONTENT-->
+    <div id="content">
+
+        <!--TATHVA LOGO-->
+            <div id="logo">
+            <section id="ticker">
+                <span data-wow-offset="80" data-wow-duration="2s" style="visibility: visible;-webkit-animation-duration: 2s;font-size:6.5em; -moz-animation-duration: 2s; animation-duration: 2s;font-family:'fontastique' !important;color:#fff;">
+                    <img src="img/logo.png" height="120px" width="120px">tathva '14
+                </span>
+                <br>
+                <span style="font-size:3em;color:white;">
+                    <span class="ticker"></span>
+                </span>
+            </section>
+            </div>
+        <!--TATHVA LOGO ENDS HERE-->
+
+        <!--DESCRIPTION TAB-->
+        
+        <!--DESCRIPTION TAB ENDS HERE-->
+
+    </div>
+    <!--CONTENT ENDS HERE-->
+
+    <!--WHYSPONSORUS TAB-->
+    <div id="whysponsorus-tab">
+        <div style="position:realtive; height:50%; width:100%; float:right; background:rgba(0,0,0,0.4);"> 
+            <ul style="color:#fff; font-family:Century Gothic; font-size:1.1em; position:relative; padding: 1em 3em 1em; ">
+                Sponsor us to be part of the largest tech fest in South India and get these benifits:
+                <li>Increase in brand visibility</li>
+                <li>Student interaction and product marketing</li>
+                <li>Banners and posters can be put up in the Open Air Theatre and other prominent locations in the campus.</li>
+                <li>Kiosks and stalls can also be allowed in various parts of the campus</li>
+                <li>Be listed on the official event web site and receive a link to your company web site.</li>
+                <li>Receive an enlarged logo on the back of the official event T-shirt</li>
+                <li>Have your enlarged logo appear on all press-printed materials including fliers or brochures and on all web ads.</li>
+            </ul>  
+        </div> 
+        <div style="position:realtive; height:50%; width:100%; float:left; background:rgba(0,0,0,0.4); padding: 0 auto">
+            <div id="stat-area">
+                <p style="padding-bottom:0.5em; font-family:Century Gothic; font-size:1.1em; color:#fff">We offer you</p>
+                <nav id="stat">
+                    <ul>
+                    <li>
+                        <a>
+                            <span>
+                                <img src="img/footfall.gif" height="80px" width="80px" style="padding-top:0px;" class="circular">
+                            </span>
+                        </a>
+                        <div>30,000+ footfall</div>
+                    </li>
+                    <li>
+                        <a>
+                            <span>
+                                <img src="img/likes.jpg" height="80px" width="80px" style="padding-top:0px;" class="circular">
+                            </span>
+                        </a>
+                        <div>21,000+ likes</div>
+                    </li>    
+                    <li>
+                        <a>
+                            <span>
+                                <img src="img/eyes.jpg" height="80px" width="80px" style="padding-top:0px" class="circular">
+                            </span>
+                        </a>
+                        <div>60,000+ eyes</div>
+                    </li>
+                </ul>
+                </nav>
+            </div>
+        </div> 
+        <div id="close-button3" style="position:absolute; right:0px; top:0px"><img src="img/close-button.png" height="30px" width="30px"></div>       
+        </div>
+        <!--WHYSPONSORUS TAB ENDS HERE-->
+
+        <!--CONTACT US TAB-->
+        <div id="contactus-tab">
+            <div class="full-width" style="padding-top:0.4em; font-size:1.3em">OUR TEAM</div>
+            <div class="full-width" style="padding-top:0.4em; font-size:1.1em">Marketting</div>
+            <div class="one-third-width" style="float:right;padding:0.2em 0 0.8em">
+                <p style="font-size:1.2em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="one-third-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="font-size:1.2em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="one-third-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="font-size:1.2em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="full-width" style="float:left;padding-top:0.8em">Public Relations</div>
+            <div class="one-half-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="font-size:1.1em">SREEJITH MURALIDHAR</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">sreejith@tathva.org</p>
+            </div>
+            <div class="one-half-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="font-size:1.1em">FAAHAD SAFEER</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">faahad@tathva.org</p>
+            </div>
+            <div class="one-quarter-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="padding-top:0.5em">Workshops</p>
+                <p style="font-size:1.1em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="one-quarter-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="padding-top:0.5em">Exhibitions</p>
+                <p style="font-size:1.1em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="one-quarter-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="padding-top:0.5em">Lectures</p>
+                <p style="font-size:1.1em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="one-quarter-width" style="float:right;padding:0.2em 0 0.8em;">
+                <p style="padding-top:0.5em">Overseas Contact</p>
+                <p style="font-size:1.1em">JOHN SMITH</p>
+                <p style="font-size:1.1em">9876543210</p>
+                <p style="font-size:1.1em">abc@tathva.org</p>
+            </div>
+            <div class="full-width" style="padding-top:0.8em 0.8em 0.8em;">for further enquiries contact <b>info@tathva.org</b></div>
+
+            <div id="close-button2" style="position:absolute; right:0px; top:0px"><img src="img/close-button.png" height="30px" width="30px"></div>       
+        </div>
+        <!--CONTACT US ENDS HERE-->
+
+        <!--ABOUT US TAB-->
+        <div id="aboutus-tab">
+            <div style="position:realtive; height:68%; width:100%; float:left; background:rgba(0,0,0,0.4);">
+                <div id="logo-area">
+                    <nav id="logo-tathva">
+                    <ul>
+                    <li>
+                        <a>
+                            <span>
+                                <img src="img/tathva.jpg" height="150px" width="150px" class="circular" >
+                            </span>
+                        </a>
+                    </li>
+                    </ul>
+                    </nav>
+                </div>
+                <div>
+                    <p style="color:white; font-family:Century Gothic; font-size:1em; padding: 0em 2em 2em; line-height:150%">
+                    &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTathva is an annual techno-management festival, organized by the National Institute of Technology Calicut at Kozhikode, in Kerala, India, in the month of October. It is one of the biggest technical festivals in South India.
+                    
+                    <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAimed at inspiring innovation, technical interest among students, and awareness among the public, we host lectures, seminars, workshops, competitions, paper presentations, exhibitions, quizzes, model displays and robotics events.
+                    
+                    <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspEvents are graded at several levels targeted at the varying interests of attending participants, who may include visiting professors, students, industry experts, delegations from peer educational institutions, and general public.</p> 
+                </div>
+            </div>
+            <div id="close-button1" style="position:absolute; right:0px; top:0px"><img src="img/close-button.png" height="30px" width="30px"></div>       
+        </div>
+        <!--ABOUT US ENDS HERE-->
+
+    <!--BACKGROUND SLIDESHOW-->
+	<div class="fluid_container">
+        <div class="fluid_dg_wrap fluid_dg_emboss pattern_1 fluid_dg_grey_skin" id="fluid_dg_wrap_4">
+            <div data-src="img/slideshow/image6.jpg"></div>
+            <div data-src="img/slideshow/image2.jpg"></div>
+            <div data-src="img/slideshow/image1.jpg"></div>
+            <div data-src="img/slideshow/image3.jpg"></div>
+        </div>
+    </div>
+    <!--BACKGROUND SLIDESHOW ENDS HERE-->
+
+    <!--BOTTOM ICONS-->
+    <div id="icons">
+        <div id="social-area">
+            <nav id="social">
+                <ul>   
+                    <li id="whysponsorus">
+                        <a href="#" title="HOME">
+                            <span >
+                                <img src="img/sponsors.png" height="40px" width="40px" style="padding-top:0.5em">
+                            </span>
+                        </a>
+                        <div>WHY SPONSOR US</div>
+                    </li>
+
+                    <li id="aboutus" >
+                        <a href="#" title="HOME">
+                            <span>
+                                <img src="img/tathva.jpg" height="70px" width="70px" class="circular">
+                            </span>
+                        </a>
+                        <div>ABOUT US</div>
+                    </li>
+                    <li id="contactus">
+                        <a href="#" title="HOME" >
+                            <span >
+                                <img src="img/contact.png" height="70px" width="70px" class="circular">
+                            </span>
+                        </a>
+                        <div>CONTACT US</div>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!--BOTTOM ICONS ENDS HERE-->
+
+    <!--FOOTER-->
+    <!--NITC ADDRESS-->
+    <div id="footer" style="position:fixed; bottom:1em;left:1em;">
+        <div id="nitc">
+            <img class="logo" src="img/nitclogo.png"></img>
+            <div id="nitc-info" style="font-size:1.2em; color:white">
+                NIT Calicut<br/> 
+                NIT Campus P. O.<br/> 
+                Calicut - 673601<br/>
+            <a target="_blank" href="http://www.nitc.ac.in/">www.nitc.ac.in</a>  
+            </div>
+        </div>
+    </div>
+    <!--COPYRIGHT-->
+    <div id="copyright" style="position:fixed; bottom:1em;right:1em; color:white; font-family:Eurostile; font-size:1.2em">
+        <span style="color:red">creative</span>@tathva.org
+    </div>
+    <!--FOOTER ENDS HERE-->
+
+
+    <!--SCRIPTS--> 
+    <!--SLIDSHOW SCRIPTS-->
+    <script type='text/javascript' src='js/jquery-1.7.2.min.js'></script>
+    <script type='text/javascript' src='js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='js/jquery.easing.1.3.js'></script> 
+    <script type='text/javascript' src='js/fluid_dg.js'></script> 
+    <script>jQuery(document).ready(function(){
+		jQuery(function(){			
+			jQuery('#fluid_dg_wrap_4').fluid_dg({height: 'auto', loader: 'bar', pagination: false, thumbnails: false, hover: false, opacityOnGrid: false, imagePath: ''});
+		}); })
+	</script>
+    <!--TICKER AND TABS SCRIPTS-->
+ 	<script type="text/javascript" src="js/scripts.js"></script>
+    <!--GALLERY SCRIPTS-->
+    <script src="js/jquery.swipebox.min.js"></script>
+    <script src="js/jquery.loadImage.js"></script>
+    <script src="js/gallery.js"></script>
+       
+</body> 
+</html>
